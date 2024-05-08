d8329f686626d8530149bcd2e15e59a19d25209c
DuyHiep <buiduyhiep0@gmail.com>
2024-05-08 15:46:54 +0700

    add not found interface

--- FE/BookStore/src/pages/404/NotFound.jsx
+++ FE/BookStore/src/pages/404/NotFound.jsx
@@ -1,6 +1,12 @@
+import { Link } from "react-router-dom"
+
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div className="flex flex-col justify-center items-center py-9">
+      <Link to='/' className="text-white w-40 bg-red-600 text-center py-3 cursor-pointer">Quay về trang chủ</Link>
+      <img className="w-1/2" srcSet="https://code.ptit.edu.vn/2020/images/download%20(1)@3x.png" alt="Not Found" />
+      <p className="text-xl font-semibold text-red-600">Not Found!</p>
+    </div>
   )
 }
 export default NotFound
